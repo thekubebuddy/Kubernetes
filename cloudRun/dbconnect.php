e6f7dd8ac4c24a0c142e52c8482c38e63570621f
ishaq4466 <shaikhishaq1996@gmail.com>
2020-03-04 23:59:44 +0530

    Added cloudRun

---
+++ cloudRun/dbconnect.php
@@ -0,0 +1,26 @@
+<?php
+//$servername = "";
+//$username = "";
+//$password = "";
+$servername = getenv('servername', true) ?: 'unknown';
+$username = getenv('username', true) ?: 'unknow-user';
+$password = getenv('password', true) ?: 'none';
+     
+     
+     
+    echo  "Connecting to the database \n";
+     echo  sprintf("Server Name: %s!\n", $servername);
+     echo  sprintf("User Name: %s!\n", $username);
+     echo  sprintf("Password: %s!\n", $password);
+
+// Create connection
+ $conn = new mysqli($servername, $username, $password);
+
+// // Check connection
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+     }
+
+     echo "DB Connected successfully";
+?>
+
